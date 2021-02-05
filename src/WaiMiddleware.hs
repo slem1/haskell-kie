62b11f792b77c95ee37d312c9d8f90e25fa5ea85
slem1 <sylvain.lem1@gmail.com>
2021-02-05 18:12:02 +0100

    wip

--- src/WaiMiddleware.hs
+++ src/WaiMiddleware.hs
@@ -10,10 +10,12 @@ import Network.Wai
 import Network.HTTP.Types
 import Network.HTTP.Types.Header
 import Network.Wai.Handler.Warp
+import Data.List
+import qualified Data.ByteString as BS
 
 import Data.Monoid (mconcat)
 
-runWebApp = run 3000 $ addLog $ addJsonContentType $ serve200Application    
+runWebApp = run 3000 $ addLog $ jwtInterceptor $ addJsonContentType $ serve200Application    
 
 serve200Application :: Application
 serve200Application request responseFn = 
@@ -32,6 +34,31 @@ addLog baseApp request responseFn = baseApp request responseFn'
         responseFn' response = do
             putStrLn "Log a response"
             responseFn response 
+
+jwtInterceptor :: Middleware 
+jwtInterceptor baseApp request responseFn = 
+    if rawPathInfo request `elem` anonymousPaths then 
+        baseApp request responseFn
+    else 
+        case hasAuthToken request of            
+            Just (_,token) -> jwtProtectedMiddleware token baseApp request responseFn
+            _ -> responseFn response401
+    where
+    hasAuthToken request = find (\(h,_) -> h == "auth-token") $ requestHeaders request
+
+
+jwtProtectedMiddleware :: BS.ByteString -> Middleware
+jwtProtectedMiddleware token baseApp request responseFn = 
+    case token of 
+        "12345" -> baseApp request responseFn
+        _ -> responseFn response401
+
+
+anonymousPaths = ["/auth/login"]
+
+response401 = responseLBS status401 [] "{ \"message\" : \"access denied\" }"
+
+
 
 
 -- myMiddleware :: Middleware
